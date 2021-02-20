521799e49881627263f19bde3140f634e0346ed1
carlos <cajimenezca@unal.edu.co>
2021-02-20 16:24:29 -0500

    React redux form

--- src/Components/ContactForm.js
+++ src/Components/ContactForm.js
@@ -1,238 +1,114 @@
-import React, { useState } from "react";
+import React from "react";
 import { Button, Form } from "react-bootstrap";
+import { Control, LocalForm, Errors } from "react-redux-form";
 
 const ContactForm = () => {
-  const userDataObj = {
-    firstname: "",
-    lastname: "",
-    telnum: "",
-    email: "",
-    agree: false,
-    contactType: "Tel.",
-    message: "",
-  };
-  const [userData, setUserData] = useState(userDataObj);
-
-  const [errors, setErrors] = useState({
-    firstname: "",
-    lastname: "",
-    telnum: "",
-    email: "",
-  });
-  const onSumbit = (e) => {
-    e.preventDefault();
-    document.getElementById("contact-form").reset();
+  const handleSubmit = (values) => {
+    console.log("values", JSON.stringify(values));
   };
 
-  const handleInputChange = (e) => {
-    let value =
-      e.target.type === "checkbox" ? e.target.checked : e.target.value;
-    let name = e.target.name;
-    setUserData({ ...userData, [name]: value });
-    validate(name);
-  };
-
-  let validate = (token) => {
-    if (token === "firstname" || token === "lastname") {
-      if (userData[token].length <= 2) {
-        return setErrors({
-          ...errors,
-          [token]:
-            token === "firstname"
-              ? "First name should be >= than 3 characters"
-              : "Last name should be >= than 3 characters",
-        });
-      } else if (userData[token].length > 9) {
-        return setErrors({
-          ...errors,
-          [token]:
-            token === "firstname"
-              ? "First name should be <= than 10 characters"
-              : "Last name should be <= than 10 characters",
-        });
-      } else {
-        return setErrors({
-          ...errors,
-          [token]: "",
-        });
-      }
-    }
-    if (token === "telnum") {
-      const regEx = /^\d+$/;
-      if (!regEx.test(userData[token])) {
-        return setErrors({
-          ...errors,
-          [token]: "Tel. number should contain only numbers",
-        });
-      } else
-        return setErrors({
-          ...errors,
-          [token]: "",
-        });
-    }
-    if (token === "email") {
-      if (userData[token].split("").filter((x) => x === "@").length !== 1) {
-        return setErrors({
-          ...errors,
-          [token]: "Email should contain a @",
-        });
-      } else
-        return setErrors({
-          ...errors,
-          [token]: "",
-        });
-    }
-  };
   return (
     <div className="row row-content">
       <h3 className="col-12">Send us your feedback</h3>
       <hr className="col-md-9" />
       <div className="col-12 col-md-9 ">
-        <Form id="contact-form">
+        <LocalForm onSubmit={(values) => handleSubmit(values)}>
           {/* first name */}
-          <Form.Group className="row align-items-center">
+          <div className="row form-group align-items-center">
             <Form.Label htmlFor="firstname" className="col-md-2">
               First name
             </Form.Label>
-            <Form.Control
-              className="col-md-10 "
-              type="text"
+            <Control.text
+              model=".firstname"
               id="firstname"
               name="firstname"
+              className="col-md-10 form-control"
               placeholder="First name"
-              isValid={errors.firstname === ""}
-              isInvalid={errors.firstname !== ""}
-              onChange={handleInputChange}
-              value={userData.firstname}
             />
-            <p
-              className={`offset-2 text-muted ${
-                errors.firstname === "" ? "d-none" : "d-block"
-              }`}
-            >
-              {errors.firstname}
-            </p>
-          </Form.Group>
+          </div>
           {/* last name */}
-          <Form.Group className="row align-items-center">
+          <div className="row form-group align-items-center">
             <Form.Label htmlFor="lastname" className="col-md-2">
               Last Name
             </Form.Label>
-            <Form.Control
-              className={`col-md-10 `}
-              isValid={errors.lastname === ""}
-              isInvalid={errors.lastname !== ""}
-              type="text"
+            <Control.text
+              model=".lastname"
               id="lastname"
               name="lastname"
+              className="col-md-10 form-control"
               placeholder="Last name"
-              onChange={handleInputChange}
-              value={userData.lastname}
             />
-            <p
-              className={`offset-2 text-muted ${
-                errors.lastname === "" ? "d-none" : "d-block"
-              }`}
-            >
-              {errors.lastname}
-            </p>
-          </Form.Group>
+          </div>
           {/* contact tel */}
-          <Form.Group className="row align-items-center">
+          <div className="row form-group align-items-center">
             <Form.Label htmlFor="telnum" className="col-md-2">
               Contact tel.
             </Form.Label>
-            <Form.Control
-              className="col-md-10"
-              type="tel"
+            <Control.text
+              model=".telnum"
               id="telnum"
               name="telnum"
+              className="col-md-10 form-control"
               placeholder="Contact tel."
-              isValid={errors.telnum === ""}
-              isInvalid={errors.telnum !== ""}
-              onChange={handleInputChange}
-              value={userData.telnum}
             />
-            <p
-              className={`offset-2 text-muted ${
-                errors.telnum === "" ? "d-none" : "d-block"
-              }`}
-            >
-              {errors.telnum}
-            </p>
-          </Form.Group>
+          </div>
           {/* email */}
-          <Form.Group className="row align-items-center">
+          <div className="row form-group align-items-center">
             <Form.Label htmlFor="email" className="col-md-2">
               Email
             </Form.Label>
-            <Form.Control
-              className="col-md-10"
+            <Control.text
+              model=".email"
+              className="col-md-10 form-control"
               type="email"
-              id="email"
               name="email"
               placeholder="Email"
-              isValid={errors.email === ""}
-              isInvalid={errors.email !== ""}
-              onChange={handleInputChange}
-              value={userData.email}
             />
-            <p
-              className={`offset-2 text-muted ${
-                errors.email === "" ? "d-none" : "d-block"
-              }`}
-            >
-              {errors.email}
-            </p>
-          </Form.Group>
+          </div>
           {/* checkbox and contact option */}
-          <Form.Group className="row align-items-center justify-content-between">
-            <Form.Check
-              className="col-md-6 offset-md-2"
-              type="checkbox"
-              name="agree"
-              label={<strong>May we contact you?</strong>}
-              checked={userData.agree}
-              onChange={handleInputChange}
-            />
-
-            <Form.Control
-              className="col-md-2"
-              as="select"
+          <div className="row form-group align-items-center justify-content-between">
+            <div className="form-check col-md-6 offset-md-2">
+              <Form.Label check>
+                <Control.checkbox
+                  model=".agree"
+                  name="agree"
+                  className="form-check-input "
+                />
+                <strong>May we contact you?</strong>
+              </Form.Label>
+            </div>
+            <Control.select
+              model=".contactType"
+              className="col-md-2 form-control"
               name="contactType"
-              onChange={handleInputChange}
-              value={userData.contactType}
             >
               <option>Tel.</option>
               <option>Email</option>
-            </Form.Control>
-          </Form.Group>
+            </Control.select>
+          </div>
           {/* textarea */}
-          <Form.Group className="row">
+          <div className="row form-group">
             <Form.Label htmlFor="feedback" className="col-md-2">
               Your feedback
             </Form.Label>
-            <Form.Control
-              className="col-md-10"
-              as="textarea"
+            <Control.textarea
+              model=".message"
               id="message"
               name="message"
+              className="col-md-10 form-control"
               rows={12}
-              onChange={handleInputChange}
-              value={userData.message}
             />
-          </Form.Group>
+          </div>
           {/* submit btn */}
-          <Form.Group className="row">
+          <div className="row form-group">
             <Button
               className="col-md-auto offset-md-2 btn btn-primary"
               type="sumbit"
-              onSubmit={onSumbit}
             >
               Send feedback
             </Button>
-          </Form.Group>
-        </Form>
+          </div>
+        </LocalForm>
       </div>
     </div>
   );
